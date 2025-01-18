9071ff120dd9e2a79e4f9fdc738dea71dee7f1e6
luke <z5363030@unsw.edu.au>
2025-01-18 22:31:49 +1100

    adds styles for the education section

--- src/components/Education/index.tsx
+++ src/components/Education/index.tsx
@@ -26,41 +26,43 @@ const Education = () => {
 
   return (
     <div id='education-container' className='home-container'>
-      <h1 className='h1 home-title'>
-        My Education
-      </h1>
-      <div>
-        {/* Education description */}
-        <p>
-          I studied a Bachelor of Computer Science at the University of New South Wales.
-          I begun my studies in february 2021, initialy studying electrical engineering
-          before picking up computer science as a double degree and later dropping down to
-          just study computer science. Through my degree i have learnt propper git tequies,
-          software design pattens, agile programing (Jira) and sql database design as well
-          as various type of test such as black box, unit, end to end and compent testing.
-        </p>
-        
-        <div>
-        {/* Notable Courses */}
-        <h3>Notable Courses</h3>
-          {
-            notableCourses.map(({ name, mark, grade, discription }, index) => {
-              return (
-                <div key={index}>
-                  {/* Course name */}
-                  <h4><b>{name}</b>{grade ? ` - ${grade}` : ''}</h4>
-                  
-                  {/* Mark */}
-                  { mark &&
-                    <h5>Mark: {mark}</h5>
-                  }
+      <div className='education-wrapper'>
+        <h1 className='h1 home-title'>
+          My Education
+        </h1>
+        <div className='education-details-container'>
+          {/* Education description */}
+          <p>
+            I studied a Bachelor of Computer Science at the University of New South Wales.
+            I begun my studies in february 2021, initialy studying electrical engineering
+            before picking up computer science as a double degree and later dropping down to
+            just study computer science. Through my degree i have learnt propper git tequies,
+            software design pattens, agile programing (Jira) and sql database design as well
+            as various type of test such as black box, unit, end to end and compent testing.
+          </p>
+          
+          <div className='notable-course-container'>
+            {/* Notable Courses */}
+            <h2>Notable Courses</h2>
+            {
+              notableCourses.map(({ name, mark, grade, discription }, index) => {
+                return (
+                  <div key={index}>
+                    {/* Course name */}
+                    <h4>{name}{grade ? ` - ${grade}` : ''}</h4>
+                    
+                    {/* Mark */}
+                    { mark &&
+                      <h5>Mark: {mark}</h5>
+                    }
 
-                  { /* Discription */ }
-                  <p>{discription}</p>
-                </div>
-              )
-            })
-          }
+                    { /* Discription */ }
+                    <p>{discription}</p>
+                  </div>
+                )
+              })
+            }
+          </div>
         </div>
       </div>
     </div>
